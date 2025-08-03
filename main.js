65f7a44e9827eb129a3af974bbce155e4bc921d9
Rick van der Wolk <rickvanderwolk@gmail.com>
2025-08-03 10:03:10 +0200

    Add styles (js)

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
 let screensaverTimeout;
 
+const screensaver = document.getElementById('js-screensaver');
+const text = document.getElementById('js-screensaver-text');
+
+screensaver.style.display = 'none';
+screensaver.style.position = 'fixed';
+screensaver.style.top = '0';
+screensaver.style.left = '0';
+screensaver.style.width = '100%';
+screensaver.style.height = '100%';
+screensaver.style.background = 'black';
+screensaver.style.zIndex = '9999';
+
 function getRandomishColor() {
     return 'hsla(' + Math.floor((Math.random() * 360)) + ', 100%, 50%, 1.0)';
 }
 
 function resetTimer() {
-    document.getElementById('js-screensaver').style.display = 'none';
+    screensaver.style.display = 'none';
     clearTimeout(screensaverTimeout);
     screensaverTimeout = setTimeout(showScreensaver, 25 * 1000);
 }
 
 function showScreensaver() {
-    document.getElementById('js-screensaver').style.display = 'block';
+    screensaver.style.display = 'block';
     animate();
     if (typeof changeLinkColors === 'function') {
         changeLinkColors();
@@ -19,12 +31,12 @@ function showScreensaver() {
 }
 
 function animate() {
-    const text =  document.getElementById('js-screensaver-text');
     let x = Math.random() * (window.innerWidth - text.clientWidth);
     let y = Math.random() * (window.innerHeight - text.clientHeight);
     let dx = 2;
     let dy = 2;
 
+    text.style.position = 'absolute';
     text.style.color = getRandomishColor();
     text.style.textShadow = `0 0 10px ${text.style.color}`;
 
@@ -48,7 +60,7 @@ function animate() {
 
         text.style.transform = `translate(${x}px, ${y}px)`;
 
-        if (document.getElementById('js-screensaver').style.display === 'block') {
+        if (screensaver.style.display === 'block') {
             requestAnimationFrame(moveText);
         }
     }
